8df63c991c7d1521168b269f1b43c60986a5f251
Deepak-vm <18vdeepak@gmail.com>
2025-12-30 17:57:50 +0530

    Changed to kando

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: 'trello-lite-attachments',
+        folder: 'Kando-attachments',
         allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'pdf', 'doc', 'docx', 'xls', 'xlsx', 'txt'],
         resource_type: 'auto'
     }
